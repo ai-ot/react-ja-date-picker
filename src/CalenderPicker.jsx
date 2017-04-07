159656623b2b8ad0016bef35f80d5bd9d7c2f5ce
Ito Maika <maika@MacBook.local>
2017-04-07 16:08:30 +0900

    CSS作成

--- src/CalenderPicker.jsx
+++ src/CalenderPicker.jsx
@@ -68,12 +68,104 @@ export default props => {
     ]
   ]
 
+  //　ここ以下どうにかする
+
+  var week = monthDays[0];
+  var week2 = monthDays[1];
+  var week3 = monthDays[2];
+  var week4 = monthDays[3];
+
+  var list = [];
+  var list2 = [];
+  var list3 = [];
+  var list4 = [];
+
+  for(var i in week){
+    list.push(<td className="calender-picker__day">{week[i].day}</td>);
+  }
+  for(var i in week2){
+    list2.push(<td className="calender-picker__day">{week2[i].day}</td>);
+  }
+  for(var i in week3){
+    list3.push(<td className="calender-picker__day">{week3[i].day}</td>);
+  }
+  for(var i in week4){
+    list4.push(<td className="calender-picker__day">{week4[i].day}</td>);
+  }
+
+
+  var num = monthDays.day;
+  console.log(num);
 
   return (
     <div className={ 'calender-wrapper' }>
-      { monthDays.map(function(week){
-        return <div>{week[0].day}</div>
-      }) }
+      <div className={ 'calender-picker__container' }>
+        <div className={ 'calender-picker__navigation' }>
+          <span　className={ 'calender-picker__navigation__button calender-picker__navigation__prev' }>←</span>
+          <span　className={ 'calender-picker__navigation__button calender-picker__navigation__next' }>→</span>
+        </div>
+
+        <div className={ 'calender-picker__month' }>
+          <div className={ 'calender-picker__caption' }>
+              <strong>{week[6].month}月</strong>
+          </div>
+          <ul className={ 'calender-picker__week' }>
+            <li>月</li>
+            <li>火</li>
+            <li>水</li>
+            <li>木</li>
+            <li>金</li>
+            <li>土</li>
+            <li>日</li>
+          </ul>
+
+          <tbody className={ 'calender-picker__month__grid' }>
+            <tr>
+              {list}
+            </tr>
+            <tr>
+              {list2}
+            </tr>
+            <tr>
+              {list3}
+            </tr>
+            <tr>
+              {list4}
+            </tr>
+          </tbody>
+        </div>
+
+        <div className={ 'calender-picker__month' }>
+          <div className={ 'calender-picker__caption' }>
+              <strong>{week[6].month + 1}月</strong>
+          </div>
+          <ul className={ 'calender-picker__week' }>
+            <li>月</li>
+            <li>火</li>
+            <li>水</li>
+            <li>木</li>
+            <li>金</li>
+            <li>土</li>
+            <li>日</li>
+          </ul>
+
+          <tbody className={ 'calender-picker__month__grid' }>
+            <tr>
+              {list}
+            </tr>
+            <tr>
+              {list2}
+            </tr>
+            <tr>
+              {list3}
+            </tr>
+            <tr>
+              {list4}
+            </tr>
+          </tbody>
+        </div>
+
+      </div>
     </div>
   )
 }
