e68c3b04ebe17b79baaeb77d0585a0d847a001fd
Rp7rf <westhouse51@gmail.com>
2017-04-10 11:56:02 +0900

    add some test

--- test/CalenderPicker.test.jsx
+++ test/CalenderPicker.test.jsx
@@ -11,7 +11,6 @@ import CalenderPicker, { CLASS_PREFIX } from '../src/CalenderPicker.jsx'
 const getClass = word => '.' + CLASS_PREFIX + word
 
 describe('Test of CalenderPicker Component', () => {
-  const CLASS_PREFIX =  'calender-picker__'
 
   it('should render .calender-wrapper', () => {
     const wrapper = shallow(<CalenderPicker />)
@@ -20,21 +19,31 @@ describe('Test of CalenderPicker Component', () => {
 
   it('should render 7 day label component', () => {
     const wrapper = shallow(<CalenderPicker />)
-    expect(wrapper.find('.' + CLASS_PREFIX + 'week-label')).to.have.length(7)
+    expect(wrapper.find(getClass('week-label'))).to.have.length(7)
   })
 
   it('should render 31 days for December', () => {
     const wrapper = shallow(<CalenderPicker date={ '2017/12/1' } />)
-    expect(wrapper.find('.' + CLASS_PREFIX + 'active')).to.be.length(31)
+    expect(wrapper.find(getClass('active'))).to.be.length(31)
   })
 
   it('should render 28 days for Febrary', () => {
     const wrapper = shallow(<CalenderPicker date={ '2017/2/1' } />)
-    expect(wrapper.find('.' + CLASS_PREFIX + 'active')).to.be.length(28)
+    expect(wrapper.find(getClass('active'))).to.be.length(28)
   })
 
   it('should render 29 days for Febrary in leap year', () => {
     const wrapper = shallow(<CalenderPicker date={ '2016/2/1' } />)
-    expect(wrapper.find('.' + CLASS_PREFIX + 'active')).to.be.length(29)
+    expect(wrapper.find(getClass('active'))).to.be.length(29)
+  })
+
+  it('caption should render 4月 for April', () => {
+    const wrapper = shallow(<CalenderPicker date={ '2017/4/1' } />)
+    expect(wrapper.find(getClass('caption')).text()).to.contains('4月')
+  })
+
+  it('caption should render 42 buttons for type button', () => {
+    const wrapper = shallow(<CalenderPicker date={ '2017/4/1' } type={ 'button' } />)
+    expect(wrapper.find('button')).to.have.length(42)
   })
 })
