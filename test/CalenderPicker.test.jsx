79391a5bd342be0e2daa195976a6293b95eb3a18
Rp7rf <westhouse51@gmail.com>
2017-04-10 16:21:41 +0900

    calendarPicker.test の引数の変更
    変更し忘れていたため追加しました.

--- test/CalenderPicker.test.jsx
+++ test/CalenderPicker.test.jsx
@@ -23,27 +23,27 @@ describe('Test of CalenderPicker Component', () => {
   })
 
   it('should render 31 days for December', () => {
-    const wrapper = shallow(<CalenderPicker date={ '2017/12/1' } />)
+    const wrapper = shallow(<CalenderPicker month={ 12 } year={ 2017 } />)
     expect(wrapper.find(getClass('active'))).to.be.length(31)
   })
 
   it('should render 28 days for Febrary', () => {
-    const wrapper = shallow(<CalenderPicker date={ '2017/2/1' } />)
+    const wrapper = shallow(<CalenderPicker month={ 2 } year={ 2017 } />)
     expect(wrapper.find(getClass('active'))).to.be.length(28)
   })
 
   it('should render 29 days for Febrary in leap year', () => {
-    const wrapper = shallow(<CalenderPicker date={ '2016/2/1' } />)
+    const wrapper = shallow(<CalenderPicker month={ 2 } year={ 2016 } />)
     expect(wrapper.find(getClass('active'))).to.be.length(29)
   })
 
   it('caption should render 4月 for April', () => {
-    const wrapper = shallow(<CalenderPicker date={ '2017/4/1' } />)
+    const wrapper = shallow(<CalenderPicker month={ 4 } year={ 2017 } />)
     expect(wrapper.find(getClass('caption')).text()).to.contains('4月')
   })
 
   it('caption should render 42 buttons for type button', () => {
-    const wrapper = shallow(<CalenderPicker date={ '2017/4/1' } type={ 'button' } />)
+    const wrapper = shallow(<CalenderPicker month={ 12 } type={ 'button' } year={ 2017 } />)
     expect(wrapper.find('button')).to.have.length(42)
   })
 })
